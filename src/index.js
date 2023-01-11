04c6a6e1d737fea25ec85c21f7c23cf9c0c34171
DEVI HARI CHARAN <haricharan12321@gmail.com>
2023-01-11 10:08:21 +0530

    types of function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function square(num) {
+  return num * num;
+}
+console.log(square(5));
+const square1 = function (num) {
+  return num * num;
+};
+console.log(square1(10));
+const arFun = (num) => num * num;
+console.log(arFun(100));
